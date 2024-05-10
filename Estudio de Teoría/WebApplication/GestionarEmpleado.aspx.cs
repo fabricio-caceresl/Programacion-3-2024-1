424391d5e280227169d0b0a91c55d14e83fefb87
fabricio-caceresl <155307949+fabricio-caceresl@users.noreply.github.com>
2024-05-10 15:33:49 -0500

    Se incluyó la funcionalidad de insertar un Empleado.

---
+++ Estudio de Teoría/WebApplication/GestionarEmpleado.aspx.cs
@@ -0,0 +1,61 @@
+﻿using RecursosHumanosController;
+using RecursosHumanosController.DAO;
+using RecursosHumanosController.MySQL;
+using RecursosHumanosModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class GestionarEmpleado : System.Web.UI.Page
+    {
+        private AreaDAO areaDao;
+        private BindingList<Area> areas;
+        private Empleado empleado;
+        private EmpleadoDAO empleadoDao;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            areaDao = new AreaMySQL();
+            areas = areaDao.listarTodas();
+            ddlAreas.DataTextField = "Nombre";
+            ddlAreas.DataValueField = "Id";
+            ddlAreas.DataSource = areas;
+            ddlAreas.DataBind();
+        }
+
+        protected void btnRegresar_Click(object sender, EventArgs e)
+        {
+            // Response.Redirect("#");
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            empleadoDao = new EmpleadoMySQL();
+
+            empleado = new Empleado();
+            empleado.Nombre = txtNombre.Text;
+            empleado.ApellidoPaterno = txtApellidoPaterno.Text;
+            empleado.Genero = rbMasculino.Checked ? 'M' : 'F';
+            empleado.FechaNacimiento = DateTime.Parse(dtpFechaNacimiento.Value);
+            empleado.Area = new Area();
+            empleado.Area.Id = Int32.Parse(ddlAreas.SelectedValue);
+            empleado.Cargo = txtCargo.Text;
+            empleado.Sueldo = Double.Parse(txtSueldo.Text);
+
+            empleadoDao.insertarEmpleado(empleado);
+
+            Response.Redirect("Home.aspx");
+        }
+    }
+}
